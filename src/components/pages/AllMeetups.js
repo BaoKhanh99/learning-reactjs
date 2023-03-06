01c715705ed67e8dcd155088e51f8f1c0b659c12
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2023-03-06 16:41:44 +0700

    Add even more components

--- src/components/pages/AllMeetups.js
+++ src/components/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../meetups/MeetupList";
+
 function AllMeetupsPage() {
   const DUMMY_DATA = [
     {
@@ -23,11 +25,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>
-        })}
-      </ul>
+      <MeetupList meetups= {DUMMY_DATA}/>
     </section>
   )
 }

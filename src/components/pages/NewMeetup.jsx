3a591c7e37bf98fa955ee6430ada166501eddfc4
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2023-03-08 11:18:06 +0700

    Send POST request

--- src/components/pages/NewMeetup.jsx
+++ src/components/pages/NewMeetup.jsx
@@ -1,10 +1,22 @@
 import NewMeetupForm from '../meetups/NewMeetUpForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData){
+    fetch(
+      'https://learning-react-66f77-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      }
+    )
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm/>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }

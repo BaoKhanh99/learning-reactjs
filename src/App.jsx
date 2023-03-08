1de68ea730f376d400ca488c2e917a23ec785cb6
tran.le.bao.khanh <SUN-ASTERISK\tran.le.bao.khanh@b123057-pc.sun-asterisk.com>
2023-03-08 09:18:49 +0700

    Create wrapper-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 import AllMeetupsPage from './components/pages/AllMeetups';
 import FavoritesPage from './components/pages/Favorites';
 import NewMeetupPage from './components/pages/NewMeetup';
 
 function App() {
   return (
-    <>
-      <MainNavigation/>
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage/>} />
         <Route path='/favorites' element={<FavoritesPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
 
